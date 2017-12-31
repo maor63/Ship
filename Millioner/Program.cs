542af107a3c9bde9a7280854f9e638eabcbbb066
maor <maor309@gmail.com>
2017-12-30 23:53:50 -0800

    Initial commit

---
+++ Millioner/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Millioner
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 5656);
+            UdpClient newsock = new UdpClient(ipep);
+
+            Console.WriteLine("Waiting for a client...");
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+
+            data = newsock.Receive(ref sender);
+
+            Console.WriteLine("Message received from {0}:", sender);
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+
+            string welcome = "IntroToNets";
+            string shipname = "BlackPurl                                 ";
+            data = Encoding.ASCII.GetBytes(welcome + shipname);
+            newsock.Send(data, data.Length, sender);
+
+//            while(true)
+//            {
+//                data = newsock.Receive(ref sender);
+//
+//                Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+//                newsock.Send(data, data.Length, sender);
+//            }
+        }
+        
+    }
+}
---
+++ Ship/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient();
+            IPEndPoint ip = new IPEndPoint(IPAddress.Broadcast, 5656);
+            byte[] bytes = Encoding.ASCII.GetBytes("Foo");
+            client.Send(bytes, bytes.Length, ip);
+            client.Close();
+        }
+
+//        static int FreeTcpPort()
+//        {
+//            TcpListener l = new TcpListener(IPAddress.Loopback, 0);
+//            l.Start();
+//            int port = ((IPEndPoint) l.LocalEndpoint).Port;
+//            l.Stop();
+//            return port;
+//        }
+//
+//        public void Send()
+//        {
+//            UdpClient client = new UdpClient();
+//            IPEndPoint ip = new IPEndPoint(IPAddress.Broadcast, 15000);
+//            byte[] bytes = Encoding.ASCII.GetBytes("Foo");
+//            client.Send(bytes, bytes.Length, ip);
+//            client.Close();
+//        }
+    }
+}
